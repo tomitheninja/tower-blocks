1bc60f91e5fd383aa6f4c710c1e389c9b1aace83
tomitheninja <suditomi@pm.me>
2019-10-23 18:19:16 +0200

    Add script to spawn cubes Add RigidBody for the cube

---
+++ Assets/Scripts/CubeSpawnBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeSpawnBehaviour : MonoBehaviour {
+
+	const int SKIP_CAMERA_MOVE_UNTIL = 3;
+
+	public GameObject cube;
+	public Button dropButton;
+	public Camera cam;
+
+	private Vector3 initialCubePosition;
+	private Quaternion initialCubeRotation;
+	private float cubeHeight;
+	private int numCubesSpawned = 1;
+
+	// Use this for initialization
+	void Start () {
+		cubeHeight = cube.GetComponent<Collider>().bounds.size.y;
+		initialCubePosition = cube.transform.position;
+		initialCubeRotation = cube.transform.rotation;
+		dropButton.onClick.AddListener(onUserSpawnEvent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) { onUserSpawnEvent(); }
+	}
+
+	/// <summary>
+	/// Handler for user generated spawn event
+	/// </summary>
+	void onUserSpawnEvent() {
+
+		Debug.Log("CubeSpawnBehaviour: spawning cube");
+		Instantiate(cube, new Vector3(
+			initialCubePosition.x,
+			initialCubePosition.y + Mathf.Max(0, numCubesSpawned - SKIP_CAMERA_MOVE_UNTIL) * cubeHeight,
+			initialCubePosition.z
+		), initialCubeRotation);
+		
+		if (numCubesSpawned > SKIP_CAMERA_MOVE_UNTIL) {
+			Debug.Log("CubeSpawnBehaviour: moving camera");
+			cam.transform.position = new Vector3(
+				cam.transform.position.x,
+				cam.transform.position.y + cubeHeight,
+				cam.transform.position.z
+			);
+		}
+
+		
+		numCubesSpawned += 1;
+	}
+}
